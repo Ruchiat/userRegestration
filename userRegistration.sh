7aea21bd5adc27eb5cf722926ee54e43d33bf215
ruchita-patil <ruchita.patil@catseyesystems.com>
2020-09-27 21:33:08 +0530

    validation for email address

--- userRegistration.sh
+++ userRegistration.sh
@@ -34,3 +34,18 @@ then
 else
 	echo "Invalid_Full_Name"
 fi
+
+#E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl& co) and 2 optional (xyz & in) 
+
+read -p "Enter The Email_ID: " Email_Address
+
+emailPat="^[a-z]+[0-9]*([-_+.][0-9a-z]+)*@[0-9a-z]+[.][a-z]{2,4}([,]{1})*([.][a-z]{2})*$"
+
+if [[ $Email_Address =~ $emailPat ]]
+then
+	echo "Valid_Information"
+else
+	echo "Invalid_Information"
+fi
+
+
